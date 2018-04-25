5ba5b56f938d81b3cc235d6b5e64b5da78f8e26b
Omer Levi Hevroni <omerlh@gmail.com>
2018-04-25 11:23:57 +0300

    initial commit

---
+++ scripts/run_security_tests.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+set -e
+
+docker-compose -f docker-compose.yaml -f docker-compose.security.yaml pull --parallel
+docker-compose -f docker-compose.yaml -f docker-compose.security.yaml run --rm glue bash /app/run_glue.sh http://api blackbox /output/logging-api.txt
